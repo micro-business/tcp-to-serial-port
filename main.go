9da6146c1016c31ec405de314cf5abb3e563530a
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-05-25 21:37:38 +1200

    Fixed bugs

--- main.go
+++ main.go
@@ -91,7 +91,6 @@ func main() {
 					if serialPort, serialError = serial.OpenPort(&serialConfig); serialError != nil {
 						log.Printf("Failed to open serial port: %s\n", serialError)
 					} else {
-						ipReadChan = make(chan readResult)
 						go readFromIO(currentConnection, ip2serialBuffer, ipReadChan, currentReadMore)
 					}
 				}
@@ -125,6 +124,7 @@ func main() {
 					}
 				}
 
+				ipReadChan = make(chan readResult)
 				currentConnection = nil
 				connectionError = nil
 				acceptMore <- true
